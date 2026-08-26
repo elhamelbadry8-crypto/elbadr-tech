4d57b23382945fd13cdbd9a4b28c696a4739bed5
elhamelbadry8-crypto <elhamelbadry8@gmail.com>
2026-08-26 04:27:37 +0300

    Update account.js

--- account/account.js
+++ account/account.js
@@ -1,5 +1,5 @@
 let currentAccountData = {}; 
-const API_BASE_URL = 'http://127.0.0.1:5000/api'; 
+const API_BASE_URL = 'https://elbadr-tecnolog.vercel.app/api'; 
 
 document.addEventListener("DOMContentLoaded", async () => {
     const token = localStorage.getItem("jwtToken");
